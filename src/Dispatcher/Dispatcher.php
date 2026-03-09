e6f753b03f93d4175fc1023d9cd7f3c5b7ef2894
Ugur Uygur <support@joomtheme.com>
2026-03-09 20:12:10 +0300

    Add files via upload

--- src/Dispatcher/Dispatcher.php
+++ src/Dispatcher/Dispatcher.php
@@ -1,1 +1,19 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_jtwhatsapp
+ *
+ * @copyright   (C) 2026 JoomTheme
+ * @license     GNU General Public License version 2 or later
+ */
+namespace JoomTheme\Module\JTWhatsApp\Site\Dispatcher;
 
+defined('_JEXEC') or die;
+
+class Dispatcher
+{
+    public function dispatch()
+    {
+        // Reserved for future logic
+    }
+}
