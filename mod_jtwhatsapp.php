b0276543c7f7f0771a1467a655d1b2a52c497264
Ugur Uygur <support@joomtheme.com>
2026-03-09 20:43:38 +0300

    Update mod_jtwhatsapp.php

--- mod_jtwhatsapp.php
+++ mod_jtwhatsapp.php
@@ -6,8 +6,5 @@
  * @copyright   (C) 2026 JoomTheme
  * @license     GNU General Public License version 2 or later
  */
+
 defined('_JEXEC') or die;
-
-use Joomla\CMS\Helper\ModuleHelper;
-
-require ModuleHelper::getLayoutPath('mod_jtwhatsapp', $params->get('layout', 'default'));
