f32366c9d4ca398f0ddcb656e67be4847e026da6
Ugur Uygur <support@joomtheme.com>
2026-03-09 20:11:04 +0300

    Add files via upload

--- services/provider.php
+++ services/provider.php
@@ -1,1 +1,24 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_jtwhatsapp
+ *
+ * @copyright   (C) 2026 JoomTheme
+ * @license     GNU General Public License version 2 or later
+ */
+defined('_JEXEC') or die;
 
+use Joomla\CMS\Extension\Service\Provider\Module as ModuleServiceProvider;
+use Joomla\CMS\Extension\Service\Provider\ModuleDispatcherFactory as ModuleDispatcherFactoryServiceProvider;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class () implements ServiceProviderInterface {
+    public function register(Container $container): void
+    {
+        $container->registerServiceProvider(
+            new ModuleDispatcherFactoryServiceProvider('\\JoomTheme\\Module\\JTWhatsApp')
+        );
+        $container->registerServiceProvider(new ModuleServiceProvider());
+    }
+};
