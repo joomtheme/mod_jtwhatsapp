bd9dad24cd885ee3627220774c240f8cca6afa77
Ugur Uygur <support@joomtheme.com>
2026-03-10 01:12:24 +0300

    Add files via upload

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,34 +6,27 @@
  * @copyright   (C) 2026 JoomTheme
  * @license     GNU General Public License version 2 or later
  */
-
 defined('_JEXEC') or die;
 
-if ($phone === '')
-{
+if ($phone === '') {
     return;
 }
 
 $link = 'https://wa.me/' . $phone;
 
-if ($message !== '')
-{
+if ($message !== '') {
     $link .= '?text=' . $message;
 }
-
-if (!in_array($position, ['right', 'left'], true))
-{
-    $position = 'right';
-}
 ?>
-
 <div class="jt-whatsapp jt-<?php echo htmlspecialchars($position, ENT_QUOTES, 'UTF-8'); ?>">
     <a
         href="<?php echo htmlspecialchars($link, ENT_QUOTES, 'UTF-8'); ?>"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="WhatsApp"
+        title="WhatsApp"
     >
-        WhatsApp
+        <span class="jt-whatsapp__icon" aria-hidden="true">✆</span>
+        <span class="jt-whatsapp__text">WhatsApp</span>
     </a>
 </div>
