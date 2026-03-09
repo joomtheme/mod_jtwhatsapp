8e2e13e9d6d066560809b31f44c2419c8e5c9f7a
Ugur Uygur <support@joomtheme.com>
2026-03-10 01:11:59 +0300

    Add files via upload

--- mod_jtwhatsapp.php
+++ mod_jtwhatsapp.php
@@ -6,22 +6,20 @@
  * @copyright   (C) 2026 JoomTheme
  * @license     GNU General Public License version 2 or later
  */
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 
-/** @var Joomla\Registry\Registry $params */
-
 $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
-$wa->registerAndUseStyle(
-    'mod_jtwhatsapp.style',
-    'modules/mod_jtwhatsapp/media/css/style.css'
-);
+$wa->registerAndUseStyle('mod_jtwhatsapp.style', 'modules/mod_jtwhatsapp/media/css/style.css');
 
 $phone = preg_replace('/\D+/', '', (string) $params->get('phone', ''));
 $message = rawurlencode((string) $params->get('message', ''));
 $position = (string) $params->get('position', 'right');
+
+if (!in_array($position, ['right', 'left'], true)) {
+    $position = 'right';
+}
 
 require ModuleHelper::getLayoutPath('mod_jtwhatsapp', $params->get('layout', 'default'));
