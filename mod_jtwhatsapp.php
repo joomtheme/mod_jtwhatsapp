d98287181b0c668a8f238bb612a73ee7d447fb09
Ugur Uygur <support@joomtheme.com>
2026-03-09 20:54:05 +0300

    Update mod_jtwhatsapp.php

--- mod_jtwhatsapp.php
+++ mod_jtwhatsapp.php
@@ -8,3 +8,20 @@
  */
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\ModuleHelper;
+
+/** @var Joomla\Registry\Registry $params */
+
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->registerAndUseStyle(
+    'mod_jtwhatsapp.style',
+    'modules/mod_jtwhatsapp/media/css/style.css'
+);
+
+$phone = preg_replace('/\D+/', '', (string) $params->get('phone', ''));
+$message = rawurlencode((string) $params->get('message', ''));
+$position = (string) $params->get('position', 'right');
+
+require ModuleHelper::getLayoutPath('mod_jtwhatsapp', $params->get('layout', 'default'));
